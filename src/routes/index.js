b4e154783cf919b354a316ef25a08cfcfb446ccb
akhancode <akhan.code@gmail.com>
2024-11-01 02:14:49 +0530

    progress api

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ const testRoute = require("./test.route");
 const authRoute = require("./auth.route");
 const habitRoute = require("./habit.route");
 const streakRoute = require("./streak.route");
+const progressRoute = require("./progress.route");
 const authMiddleware = require("../middleware/authMiddlware");
 
 
 router.use("/auth",authRoute);
 router.use("/api/habit",authMiddleware,habitRoute);
 router.use("/api/streak",authMiddleware,streakRoute);
+router.use("/api/progress",authMiddleware,progressRoute);
 router.use("/api",authMiddleware,testRoute);
 
 
